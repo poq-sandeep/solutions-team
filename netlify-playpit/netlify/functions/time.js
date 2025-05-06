178fa7952483fe84a0e1c0038f99b18c17f42aaa
Sandeep Singh <sandeep@sandeeps-mbp.home>
2025-05-06 21:31:17 +0100

    push

--- netlify-playpit/netlify/functions/time.js
+++ netlify-playpit/netlify/functions/time.js
@@ -22,47 +22,48 @@ exports.handler = async (event, context) => {
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
-			"time": timeString,
-			"resourceManifest": null,
-			"id": "d0b0c47a-dec3-4510-93ae-14f61dcc0efd",
-			"component": {
-				"type": "vertical-scroll",
-				"content": {
-					"components": [{
-						"content": {
-							A4g45t2020 "title": "Color",
-							"items": [{
-								"title": "Yellow",
-								"link": "content/ef5ba68c-fcda-44a2-b2ee-c061587bf405",
-								"imageUrl": "https://poqmbuat.blob.core.windows.net/app292/45585842-1.png?v=133910178050860000"
-							}, {
-								"title": "Beige",
-								"link": "content/d0b0c47a-dec3-4510-93ae-14f61dcc0efd",
-								"imageUrl": "https://poqmbuat.blob.core.windows.net/app292/45585843-1.png?v=133910178295760000"
-							}, {
-								"title": "Golden",
-								"link": "content/d0b0c47a-dec3-4510-93ae-14f61dcc0efd",
-								"imageUrl": "https://poqmbuat.blob.core.windows.net/app292/45585844-1.png?v=133910178550590000"
-							}],
-							"customData": {
-								"carousel-type": "swatch"
-							}
-						},
-						"modifiers": {
-							"padding": {
-								"top": 0,
-								"bottom": 0,
-								"left": 0,
-								"right": 0
-							}
-						},
-						"type": "image-carousel"
-					}]
-				},
-				"modifiers": {},
-				"customData": null
-			},
-			"customData": null
-		})
+			"time": timeString})
+			// // "resourceManifest": null,
+// // 			"id": "d0b0c47a-dec3-4510-93ae-14f61dcc0efd",
+// // 			"component": {
+// // 				"type": "vertical-scroll",
+// // 				"content": {
+// // 					"components": [{
+// // 						"content": {
+// // 							A4g45t2020 "title": "Color",
+// // 							"items": [{
+// // 								"title": "Yellow",
+// // 								"link": "content/ef5ba68c-fcda-44a2-b2ee-c061587bf405",
+// // 								"imageUrl": "https://poqmbuat.blob.core.windows.net/app292/45585842-1.png?v=133910178050860000"
+// // 							}, {
+// // 								"title": "Beige",
+// // 								"link": "content/d0b0c47a-dec3-4510-93ae-14f61dcc0efd",
+// // 								"imageUrl": "https://poqmbuat.blob.core.windows.net/app292/45585843-1.png?v=133910178295760000"
+// // 							}, {
+// // 								"title": "Golden",
+// // 								"link": "content/d0b0c47a-dec3-4510-93ae-14f61dcc0efd",
+// // 								"imageUrl": "https://poqmbuat.blob.core.windows.net/app292/45585844-1.png?v=133910178550590000"
+// // 							}],
+// // 							"customData": {
+// // 								"carousel-type": "swatch"
+// // 							}
+// // 						},
+// // 						"modifiers": {
+// // 							"padding": {
+// // 								"top": 0,
+// // 								"bottom": 0,
+// // 								"left": 0,
+// // 								"right": 0
+// // 							}
+// // 						},
+// // 						"type": "image-carousel"
+// // 					}]
+// // 				},
+// // 				"modifiers": {},
+// // 				"customData": null
+// // 			},
+// // 			"customData": null
+// // 		}
+// 	)
 	};
 };
